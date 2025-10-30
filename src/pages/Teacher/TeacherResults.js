9abb7e4feeb19f3768d9c07ea6f5cccab290c2ce
JayBee <182414749+jithinboj@users.noreply.github.com>
2025-10-30 22:12:53 +0530

    Create TeacherResults.js

---
+++ src/pages/Teacher/TeacherResults.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TeacherResults = () => {
+    // This component will be implemented later.
+    // It would fetch results for quizzes created by this teacher.
+    return (
+        <div>
+            <h1>Student Results</h1>
+            <p>This page will display the results of students who have taken your quizzes.</p>
+            {/* TODO: Fetch and display results data */}
+        </div>
+    );
+};
+
+export default TeacherResults;
